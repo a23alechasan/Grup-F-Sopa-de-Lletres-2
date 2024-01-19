b5625e0a4a0196ba971ee98c059c1acc343b7731
a23alechasan <a23alechasan@inspedralbes.cat>
2024-01-19 10:33:37 +0100

    el projecte com al principi, feu les vostres branques i feu copia i pega del que ja tenieu

---
+++ src/SopaDeLLetres.java
@@ -0,0 +1,18 @@
+public class SopaDeLLetres {
+    public static void main(String[] args) {
+        System.out.println("test");
+        /*
+        (Fer Funcio main) -Alex
+        >demanar lletres (min max 100) -Iker (demanarLletres)
+        >convertir-les a matriu 10x10 -Marc (lletresAMatriu)
+        >imprimir matriu -Adrian (ImprimirMatriu)
+        >solicitar paraules while (!true)
+        -->descartar si la paraula introduida < 3 caracters i > 10 caracters
+        -->detectar si la paraula introduida es troba (horitzontal)
+        -->detectar si la paraula introduida es troba (vertical)
+        -->pintar paraules de vermell quan son trobades
+        -->identificar si les 5 paraules s'han trobat
+        >while (!true) s'acaba*/
+
+    }
+}
