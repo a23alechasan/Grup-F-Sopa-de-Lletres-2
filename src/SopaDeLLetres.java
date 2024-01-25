5027e9f5172865431d69def42f8fe09deb3d95f0
IkerDelgado <a23ikedelgra@inspedralbes.cat>
2024-01-25 10:42:13 +0100

    mateix push que l'anterior repositori + definició funcions en angles

--- src/SopaDeLLetres.java
+++ src/SopaDeLLetres.java
@@ -1,18 +1,59 @@
+import java.util.Scanner;
+
 public class SopaDeLLetres {
     public static void main(String[] args) {
-        System.out.println("test");
-        /*
-        (Fer Funcio main) -Alex
-        >demanar lletres (min max 100) -Iker (demanarLletres)
-        >convertir-les a matriu 10x10 -Marc (lletresAMatriu)
-        >imprimir matriu -Adrian (ImprimirMatriu)
-        >solicitar paraules while (!true)
-        -->descartar si la paraula introduida < 3 caracters i > 10 caracters
-        -->detectar si la paraula introduida es troba (horitzontal)
-        -->detectar si la paraula introduida es troba (vertical)
-        -->pintar paraules de vermell quan son trobades
-        -->identificar si les 5 paraules s'han trobat
-        >while (!true) s'acaba*/
+        char[][] matriu = demanarLletres();
+        mostrarMatriu(matriu);
+    }
 
+    /**
+     * Request a String to check if there are 100 letters.
+     * @return letters to the function convertirLletresMatriu
+     */
+
+    private static char[][] demanarLletres() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Dona 100 lletres seguides per començar el joc:");
+        String lletres = input.nextLine();
+        for(int i = 0; i < 100; i++) {
+            if (lletres.length() != 100) {
+                System.out.println("No hi han 100 lletres, torna a provar");
+                lletres = input.nextLine();
+            }
+
+        }
+        return convertirLletresMatriu(lletres);
+    }
+
+    /**
+     * Convert and separate the 100 letters in a 10*10 matrix
+     * @param lletres String where the 100 letters are saved
+     * @return matriu Final result of the converted matriu
+     */
+
+    public static char[][] convertirLletresMatriu(String lletres) {
+        char[][] matriu = new char[10][10];
+        int index = 0;
+
+        for (int f = 0; f < 10; f++) {
+            for (int c = 0; c < 10; c++) {
+                matriu[f][c] = lletres.charAt(index++);
+            }
+        }
+
+        return matriu;
+    }
+
+    /**
+     * Show the matrix in the sceen
+     * @param matriu char to show
+     */
+    public static void mostrarMatriu(char[][] matriu) {
+        for (int f = 0; f < 10; f++) {
+            for (int c = 0; c < 10; c++) {
+                System.out.print(matriu[f][c] + " ");
+            }
+            System.out.println();
+        }
     }
 }
