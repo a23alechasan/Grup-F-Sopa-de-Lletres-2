98a336d938c7c069811129f834ee98341943cc19
a23alechasan <a23alechasan@inspedralbes.cat>
2024-01-26 10:39:32 +0100

    cosas añadidas

--- src/SopaDeLLetres.java
+++ src/SopaDeLLetres.java
@@ -1,11 +1,33 @@
 import java.util.Scanner;
 
 public class SopaDeLLetres {
-    public static void main(String[] args) {
-        char[][] matriu = demanarLletres();
-        mostrarMatriu(matriu);
+    public static void main(String[] args){
+        System.out.println("Sisplau, introdueix les lletres de la sopa de lletres");
+        int paraulesContades = 0;
+        String lletres = demanarLletres();
+        //char[][] matriu = lletresAMatriu(lletres);
+        //imprimirMatriu(lletres);
+        while (paraulesContades < 5) {
+            System.out.println("Introdueix una paraula per comprovar si existeix.");
+            String paraula = solicitarParaula();
+            char[] lletresParaula = separarParaula(paraula);
+            while (!esValida(lletresParaula)){
+                System.out.println("Sisplau, introdueix una paraula vàlida.");
+                paraula = solicitarParaula();
+                lletrdetectarHoritzontalesParaula = separarParaula(paraula);
+            }
+            //detectarHoritzontal(matriu, lletresParaula);
+            //detectarVertical(matriu, lletresParaula);
+            //if (coordenades[0][0] != void){
+            //pintarParaules(coordenades[0[0], coordenades[1][1]);
+            //imprimirMatriu(matriu);
+            paraulesContades++;
+            //} else {
+            //imprimirMatriu(matriu);
+            //}
+            //}
+        }
     }
-
     /**
      * Request a String to check if there are 100 letters.
      * @return letters to the function convertirLletresMatriu
@@ -24,7 +46,6 @@ public class SopaDeLLetres {
         }
         return convertirLletresMatriu(lletres);
     }
-
     /**
      * Convert and separate the 100 letters in a 10*10 matrix
      * @param lletres String where the 100 letters are saved
@@ -54,6 +75,28 @@ public class SopaDeLLetres {
                 System.out.print(matriu[f][c] + " ");
             }
             System.out.println();
+        }
+    }
+
+    public static String solicitarParaula(){
+        Scanner input = new Scanner(System.in);
+        return input.nextLine();
+    }
+    public static char[] separarParaula(String paraula){
+        char[] lletresParaula;
+        lletresParaula = paraula.toCharArray();
+        return lletresParaula;
+    }
+    public  static boolean esValida(char[] lletres){
+        return lletres.length >= 3 && lletres.length <= 10;
+    }
+    public static void detectarHoritzontal(char[][] matriu, char[] lletresParaula){
+        for (int x = 0; x < matriu.length; x++){
+            for (int y = 0; y < matriu.length; y++){
+                if (matriu[x][y] == lletresParaula[0]){
+                    for (int i = 0; )
+                }
+            }
         }
     }
 }
