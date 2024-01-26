208e6a6b27bd9773f127240a801ff7ab9cad7bf1
IkerDelgado <a23ikedelgra@inspedralbes.cat>
2024-01-26 12:19:39 +0100

    NUEVO MAIN CON TODO EL CODIGO MENOS QUE LAS PALABRAS SE REPITAN

--- src/SopaDeLLetres.java
+++ src/SopaDeLLetres.java
@@ -1,33 +1,32 @@
 import java.util.Scanner;
 
 public class SopaDeLLetres {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
         System.out.println("Sisplau, introdueix les lletres de la sopa de lletres");
-        int paraulesContades = 0;
-        String lletres = demanarLletres();
-        //char[][] matriu = lletresAMatriu(lletres);
-        //imprimirMatriu(lletres);
-        while (paraulesContades < 5) {
-            System.out.println("Introdueix una paraula per comprovar si existeix.");
-            String paraula = solicitarParaula();
-            char[] lletresParaula = separarParaula(paraula);
-            while (!esValida(lletresParaula)){
-                System.out.println("Sisplau, introdueix una paraula vàlida.");
-                paraula = solicitarParaula();
-                lletrdetectarHoritzontalesParaula = separarParaula(paraula);
+        char[][] matriu = demanarLletres();
+        mostrarMatriu(matriu);
+        int palabrasEncontradas = 0;
+        while (palabrasEncontradas < 5){
+            System.out.println("Escriu una paraula :");
+            String palabra = sc.nextLine().toUpperCase();
+
+            if (palabra.length() > 3 && palabra.length() <= 10) {
+                if (buscarPalabra(matriu, palabra)) {
+                    palabrasEncontradas++;
+                    System.out.println("Paraules trobades: " + palabrasEncontradas);
+                    pintarEnRojo(matriu);
+                } else {
+                    System.out.println("La paraula " + palabra + " no està a la sopa de lletres.");
+                }
+            } else {
+                System.out.println("La longitud de la paraula ha de ser entre 3 i 10 lletras.");
             }
-            //detectarHoritzontal(matriu, lletresParaula);
-            //detectarVertical(matriu, lletresParaula);
-            //if (coordenades[0][0] != void){
-            //pintarParaules(coordenades[0[0], coordenades[1][1]);
-            //imprimirMatriu(matriu);
-            paraulesContades++;
-            //} else {
-            //imprimirMatriu(matriu);
-            //}
-            //}
         }
+
+        System.out.println("Totes les paraules s'han trobat");
     }
+
     /**
      * Request a String to check if there are 100 letters.
      * @return letters to the function convertirLletresMatriu
@@ -37,7 +36,7 @@ public class SopaDeLLetres {
         Scanner input = new Scanner(System.in);
         System.out.println("Dona 100 lletres seguides per començar el joc:");
         String lletres = input.nextLine();
-        for(int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++) {
             if (lletres.length() != 100) {
                 System.out.println("No hi han 100 lletres, torna a provar");
                 lletres = input.nextLine();
@@ -45,6 +44,7 @@ public class SopaDeLLetres {
         }
         return convertirLletresMatriu(lletres);
     }
+
     /**
      * Convert and separate the 100 letters in a 10*10 matrix
      * @param lletres String where the 100 letters are saved
@@ -62,6 +62,7 @@ public class SopaDeLLetres {
         }
         return matriu;
     }
+
     /**
      * Show the matrix in the sceen
      * @param matriu char to show
@@ -75,25 +76,57 @@ public class SopaDeLLetres {
         }
     }
 
-    public static String solicitarParaula(){
-        Scanner input = new Scanner(System.in);
-        return input.nextLine();
-    }
-    public static char[] separarParaula(String paraula){
-        char[] lletresParaula;
-        lletresParaula = paraula.toCharArray();
-        return lletresParaula;
-    }
-    public  static boolean esValida(char[] lletres){
-        return lletres.length >= 3 && lletres.length <= 10;
-    }
-    public static void detectarHoritzontal(char[][] matriu, char[] lletresParaula){
-        for (int x = 0; x < matriu.length; x++){
-            for (int y = 0; y < matriu.length; y++){
-                if (matriu[x][y] == lletresParaula[0]){
-                    for (int i = 0; )
+    static boolean buscarPalabra(char[][] matriu, String palabra) {
+        for (int i = 0; i < matriu.length; i++) {
+            for (int j = 0; j < matriu[0].length; j++) {
+                if (buscarPalabraEnDireccion(matriu, palabra, i, j, 0, 1) ||
+                        buscarPalabraEnDireccion(matriu, palabra, i, j, 1, 0) ||
+                        buscarPalabraEnDireccion(matriu, palabra, i, j, 1, 1) ||
+                        buscarPalabraEnDireccion(matriu, palabra, i, j, 1, -1)) {
+                    return true;
                 }
             }
         }
+        return false;
+    }
+    static boolean buscarPalabraEnDireccion(char[][] matriu, String palabra, int fila, int columna, int dirFila, int dirColumna) {
+        int longitud = palabra.length();
+        int nuevaFila = fila + (longitud - 1) * dirFila;
+        int nuevaColumna = columna + (longitud - 1) * dirColumna;
+
+        if (nuevaFila >= 0 && nuevaFila < matriu.length && nuevaColumna >= 0 && nuevaColumna < matriu[0].length) {
+            for (int k = 0; k < longitud; k++) {
+                char letra = matriu[fila + k * dirFila][columna + k * dirColumna];
+                if (Character.isUpperCase(letra) && letra != palabra.charAt(k)) {
+                    return false;
+                }
+            }
+
+            for (int k = 0; k < longitud; k++) {
+                matriu[fila + k * dirFila][columna + k * dirColumna] = Character.toLowerCase(matriu[fila + k * dirFila][columna + k * dirColumna]);
+            }
+            return true;
+        }
+
+        return false;
+    }
+    static void pintarEnRojo(char[][] matriu) {
+        System.out.println("Matriz amb les paraules marcades en vermell:");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                char c = matriu[i][j];
+                if (Character.isLowerCase(c)) {
+                    System.out.print("\u001B[31m" + Character.toUpperCase(c) + "\u001B[0m" + " ");
+                } else {
+                    System.out.print(c + " ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
+
+
+
+
